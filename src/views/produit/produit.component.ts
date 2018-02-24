43909ecf5fe2d94abbaa9cff29e80182d94dfd19
Delehaye Alexis <alexis_delehaye@ens.univ-artois.fr>
2018-02-23 19:55:06 -0500

    2ème push du laboratoire 2 de technologie web avancée

--- src/views/produit/produit.component.ts
+++ src/views/produit/produit.component.ts
@@ -6,9 +6,24 @@ import {ActivatedRoute} from '@angular/router';
 
 
 @Component({
-  selector :'app-root',
+  selector : 'app-root',
   templateUrl : './produit.component.html',
   styleUrls: [ './produit.component.css' ]
-
-
 })
+
+export class ProduitDetailsComponent {
+  private model: Produit;
+  public listeProduit: Produit[] = [
+    new Produit('sacoche', 'sacoche en cuir', ' c\'est une sacoche en cuir cher !', 4000),
+    new Produit(' pantalon', 'pantalon noir ', ' c\'est un beau pantalon noir', 20)
+  ];
+
+  constructor(private route: ActivatedRoute) {
+    let myId = '';
+    this.route.params.subscribe(params => {
+      myId = params['id'];
+    });
+
+    this.model = this.listeProduit[myId];
+  }
+}
