e0c7374c92a71661704c21c4e34c3ae92d6e1aa3
Delehaye Alexis <alexis_delehaye@ens.univ-artois.fr>
2018-02-23 19:44:54 -0500

    1er push du laboratoire 2 de technologie web avancée

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
+import {Produit} from '../models/Produit';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class  AppComponent {
+  public listeProduit: Produit[] = [
+    new Produit('sacoche', 'sacoche en cuir', ' c\'est une sacoche en cuir cher !', 4000),
+    new Produit(' pantalon', 'pantalon noir ', ' c\'est un beau pantalon noir', 20)
+  ];
 }
---
+++ src/views/produit/produit.component.ts
@@ -0,0 +1,14 @@
+import {Produit} from '../../models/Produit';
+import { Component } from '@angular/core';
+
+
+import {ActivatedRoute} from '@angular/router';
+
+
+@Component({
+  selector :'app-root',
+  templateUrl : './produit.component.html',
+  styleUrls: [ './produit.component.css' ]
+
+
+})
